651bd59f2092b35a6a304bf78a65f90713639d58
adhinnnugroho <adhinnnugroho@gmail.com>
2024-02-21 07:51:35 +0700

    integrate a prisma to connect mysql database

---
+++ prisma/migrations/20240220141152_init/migration.sql
@@ -0,0 +1,10 @@
+-- CreateTable
+CREATE TABLE `list_menu` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `icons` VARCHAR(255) NOT NULL,
+    `Name` VARCHAR(191) NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
