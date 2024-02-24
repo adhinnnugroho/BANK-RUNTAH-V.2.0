102a22160c9c7cff65499f9822b2a41d2a6cf454
adhinnnugroho <adhinnnugroho@gmail.com>
2024-02-25 06:23:18 +0700

    refactor: structure folder UI

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,9 @@
 import { Inter } from "next/font/google";
 import AppLayouts from "@/Layouts";
-import ListMenu from "@/UI/listmenu";
-import RuntahCoin from "@/UI/RuntahCoin";
-import SlidePromo from "@/UI/Slider";
-import ListTrash from "@/UI/Trash";
+import ListMenu from "@/UI/Home/listmenu";
+import RuntahCoin from "@/UI/Home/RuntahCoin";
+import SlidePromo from "@/UI/Home/Slider";
+import ListTrash from "@/UI/Home/Trash";
 
 const inter = Inter({ subsets: ["latin"] });
 
