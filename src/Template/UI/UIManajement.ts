cc2e9efa689085962ca2a7d0854eb9a87382e7c9
adhinnnugroho <adhinnnugroho@gmail.com>
2024-06-16 20:24:26 +0700

    feat: setup ui manajemen for reusable code

---
+++ src/Template/UI/UIManajement.ts
@@ -0,0 +1,13 @@
+import MainMenu from "@/UI/Home/main-menu";
+import RuntahCoin from "@/UI/Home/runtah-coin";
+import HomeCarousel from "@/UI/Home/home-carousel";
+import ListTrash from "@/UI/Home/list-trash";
+
+const UIManajement  = {
+    MainMenu,
+    RuntahCoin,
+    HomeCarousel,
+    ListTrash
+}
+
+export default UIManajement
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,28 +1,13 @@
-import { Inter } from "next/font/google";
+
 import AppLayouts from "@/Template/Layouts";
-import MainMenu from "@/Template/UI/Home/main-menu";
-import RuntahCoin from "@/Template/UI/Home/runtah-coin";
-import HomeCarousel from "@/Template/UI/Home/home-carousel";
-import ListTrash from "@/Template/UI/Home/list-trash";
-
-const inter = Inter({ subsets: ["latin"] });
-
-
-
+import UIManajement from "@/Template/UI/UIManajement";
 export default function Home() {
   return (
-    <>
       <AppLayouts>
-        
-        <RuntahCoin />
-        <HomeCarousel />
-
-        <MainMenu />
-
-
-        {/* 
-        <ListTrash />*/}
+        <UIManajement.RuntahCoin />
+        <UIManajement.HomeCarousel />
+        <UIManajement.MainMenu />
+        <UIManajement.ListTrash />
       </AppLayouts>
-    </>
   );
 }
